e9690444fc5bdb0211533126baa427e0afa6d44a
rogersds <rogger_souza@hotmail.com>
2025-01-11 12:18:24 -0300

    add imagens e consumindo a API de previsão do tempo

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'São Paulo';
+  weatherDatas!: WeatherDatas;
 
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWheatherDatas(this.initialCityName);
+  }
+
+  getWheatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName).subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response);
+        console.log(this.weatherDatas)
+      },
+      error: (error) => console.log(error),
+    });
+  }
 }
